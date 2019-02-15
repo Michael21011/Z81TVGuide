8df4a61089d957c4087d3f6fde81bac78d3cdd89
Michael Shapiro <michael_shapiro@epam.com>
2019-02-15 18:14:57 +0300

    v.67 Removing Firebird due deletion from market

--- app/src/main/java/info/z81/z81tvguide/Analitic.java
+++ app/src/main/java/info/z81/z81tvguide/Analitic.java
@@ -3,12 +3,11 @@ package info.z81.z81tvguide;
 import android.app.Application;
 import android.os.Bundle;
 
-import com.google.firebase.analytics.FirebaseAnalytics;
 
 import static info.z81.z81tvguide.Z81TVGuide.getAppContext;
 
 public class Analitic {
-    private FirebaseAnalytics mFirebaseAnalytics;
+   // private FirebaseAnalytics mFirebaseAnalytics;
 
 
 
@@ -19,7 +18,7 @@ public class Analitic {
     }
 
     private Analitic() {
-        mFirebaseAnalytics = FirebaseAnalytics.getInstance(getAppContext());
+       // mFirebaseAnalytics = FirebaseAnalytics.getInstance(getAppContext());
 
 
 
@@ -27,30 +26,33 @@ public class Analitic {
 
     public void NewScreen(String screenName)
     {
+        /*
         Bundle params = new Bundle();
         params.putString(FirebaseAnalytics.Param.ITEM_CATEGORY, "screen");
         params.putString(FirebaseAnalytics.Param.ITEM_NAME, screenName);
         mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.VIEW_ITEM, params);
-
+*/
 
 
     };
 
     public void NewClick(String activityName, String clickName)
     {
+        /*
         Bundle params = new Bundle();
         params.putString(FirebaseAnalytics.Param.ITEM_CATEGORY, activityName);
         params.putString(FirebaseAnalytics.Param.ITEM_NAME, clickName);
         mFirebaseAnalytics.logEvent("Click",  params);
-
+*/
     };
 
     public void NewEvent(String eventName)
-    {
+    {/*
         Bundle params = new Bundle();
         params.putString(FirebaseAnalytics.Param.ITEM_CATEGORY, "Event");
         params.putString(FirebaseAnalytics.Param.ITEM_NAME, eventName);
         mFirebaseAnalytics.logEvent("Event",  params);
+        */
     };
 
 }
